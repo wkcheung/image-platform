e55f3993251bedafae59023355348ae010a1d01f
Amets Cheung <ametscheung@Ametss-MacBook-Pro.local>
2018-03-06 11:20:51 +0000

    Working on masonry display

--- src/components/Image/ImageBoard.js
+++ src/components/Image/ImageBoard.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import ImageCard from './ImageCard'
+import ImageCard from './ImageCard';
+import {
+  CellMeasurer,
+  CellMeasurerCache,
+  createMasonryCellPositioner,
+  Masonry
+} from 'react-virtualized';
+
+// Array of images with captions
+const imageList = [
+  {
+    source: 'the-end-of-the-f-world.jpg'
+  },
+  {
+    source: 'the-end-of-the-f-world.jpg'
+  },
+  {
+    source: 'game-of-thrones.jpg'
+  }
+];
+
+// Default sizes help Masonry decide how many images to batch-measure
+const cache = new CellMeasurerCache({
+  defaultHeight: 620,
+  defaultWidth: 400,
+  fixedWidth: true
+})
+
+// Our masonry layout will use 3 columns with a 10px gutter between
+const cellPositioner = createMasonryCellPositioner({
+  cellMeasurerCache: cache,
+  columnCount: 3,
+  columnWidth: 400,
+  spacer: 10
+})
+
+function cellRenderer ({ index, key, parent, style }) {
+  const datum = imageList[index];
+  return (
+    <CellMeasurer
+      cache={cache}
+      index={index}
+      key={key}
+      parent={parent}
+    >
+      <ImageCard style={style} src={datum.source} />
+    </CellMeasurer>
+  )
+}
 
 class ImageBoard extends Component {
   render() {
     return(
       <div className="image-card-container">
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
-        <ImageCard src={require('../../assets/images/game-of-thrones.jpg')} />
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
-        <ImageCard src={require('../../assets/images/game-of-thrones.jpg')} />
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
-        <ImageCard src={require('../../assets/images/the-end-of-the-f-world.jpg')} />
+        <Masonry
+          autoHeight={false}
+          cellCount={imageList.length}
+          cellMeasurerCache={cache}
+          cellPositioner={cellPositioner}
+          cellRenderer={cellRenderer}
+          height={1000}
+          width={1000}
+        />
+        <ImageCard src={imageList[0].source} />
       </div>
     );
   }
--- src/components/Image/ImageCard.js
+++ src/components/Image/ImageCard.js
@@ -3,30 +3,29 @@ import { Card, Icon } from 'semantic-ui-react'
 
 class ImageCard extends Component {
   render() {
+    const imageUrl = require(`../../assets/images/${this.props.src}`);
     return (
-      <div style={{position: 'relative'}}>
-        <div className="image-card" style={{position: 'absolute'}}>
-          <Card centered fluid>
-            <Card.Content>
-              <Card.Header>The End of the F World</Card.Header>
-            </Card.Content>
-            <div style={{overflow: 'hidden'}}>
-              <div style={{maxHeight: '800px'}}>
-                <img src={this.props.src} />
-              </div>
+      <div className="image-card" style={this.props.style}>
+        <Card centered fluid>
+          <Card.Content>
+            <Card.Header>The End of the F World</Card.Header>
+          </Card.Content>
+          <div style={{overflow: 'hidden'}}>
+            <div style={{maxHeight: '600px'}}>
+              <img src={imageUrl} />
             </div>
-            <Card.Content>
-              <Card.Meta>28 February 2018</Card.Meta>
-              <Card.Description>A show from Netflix</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name='user' />
-                10 Friends
-              </a>
-            </Card.Content>
-          </Card>
-        </div>
+          </div>
+          <Card.Content>
+            <Card.Meta>28 February 2018</Card.Meta>
+            <Card.Description>A show on Netflix</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              10 Friends
+            </a>
+          </Card.Content>
+        </Card>
       </div>
     )
   }
